370a5b883539150f821e22cc4d6216dd0ce65c0b
SGUDestiny <panormov2006@gmail.com>
2026-06-24 05:53:31 +0400

    Some beginnings

---
+++ src/main/java/destiny/null_ouroboros/server/block/ElectromagneticAssemblyBlock.java
@@ -0,0 +1,7 @@
+package destiny.null_ouroboros.server.block;
+
+public class ElectromagneticAssemblyBlock extends ComputerPeripheralBlock {
+    public ElectromagneticAssemblyBlock(Properties properties) {
+        super(properties);
+    }
+}
---
+++ src/main/java/destiny/null_ouroboros/server/block/StopSignBlock.java
@@ -0,0 +1,9 @@
+package destiny.null_ouroboros.server.block;
+
+import net.minecraft.world.level.block.Block;
+
+public class StopSignBlock extends Block {
+    public StopSignBlock(Properties properties) {
+        super(properties);
+    }
+}
